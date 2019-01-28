d45645d03f03d534a9096d30cb1bfa72ce97d97f
Kevin Wijaya <kevin.tanuhardi@gmail.com>
2019-01-28 14:13:20 +0700

    rev User model

---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+const User = require('../models/User')
+
+module.exports = {
+  create: function(req,res,next){
+
+  }
+}
--- models/User.js
+++ models/User.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const usersSchema = new Schema({
-  title: String,
-  poster_path: String,
-  overview: String,
-  popularity: Number,
-  tag: []
+  gameId: String,
+  highScore: Number,
 })
 
 let User = mongoose.model('User', usersSchema)
