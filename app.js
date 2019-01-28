b6c1f4f04c930f648e73052e48128498cdb46ab0
Kevin Wijaya <kevin.tanuhardi@gmail.com>
2019-01-28 15:13:33 +0700

    changed to express generator

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://kevin.tanuhardi:passwordHacktiv8@ds213645.mlab.com:13645/vroggy')
 var db = mongoose.connection;
@@ -10,16 +11,31 @@ db.once('open', function() {
   console.log("we're connected!")
 });
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+var usersRouter = require('./routes/users');
 
-//ROUTER
-const usersRouter = require('./routes/users')
+var app = express();
 
-app.use('/users', usersRouter)
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () =>{
-  console.log('Server running on port ' + port)
-})
+app.use('/users', usersRouter);
 
-module.exports = app
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
